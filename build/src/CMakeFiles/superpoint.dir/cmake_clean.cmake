03b80d4368aec88b79421fd069fbca9019a64eb5
zoeyuchao <zoeyuchao@1625360790@qq.com>
2019-07-28 12:59:59 +0800

    add example

---
+++ build/src/CMakeFiles/superpoint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/superpoint.dir/SuperPoint.cpp.o"
+  "../../lib/libsuperpoint.pdb"
+  "../../lib/libsuperpoint.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/superpoint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
